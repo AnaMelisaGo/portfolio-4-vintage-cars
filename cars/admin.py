c027930f2ca651552de2fbe8a1b9dec53873e658
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-12 20:01:15 +0000

    Register the models

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PostCar, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+admin.site.register(PostCar)
+admin.site.register(Comment)
