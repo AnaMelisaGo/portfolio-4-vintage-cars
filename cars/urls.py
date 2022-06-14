12d04475f1990af3f08c633ecdb88f739ec0fddf
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-14 00:51:53 +0000

    Create car.html and add view to render posts in the template

--- cars/urls.py
+++ cars/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('cars/', views.CarPageView.as_view(), name='cars')
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic, View
+from django.core.paginator import Paginator
 from .models import PostCar
 
 
@@ -14,11 +15,19 @@ class HomePageView(View):
         return render(request, 'home/index.html', context)
 
 
-class PostCarList(generic.ListView):
+class CarPageView(View):
     """ Render the list of posted cars and display them to any user
-    and separate the posts in pages, limiting the number of car posts in 6
-    in each page """
-    model = PostCar
-    queryset = PostCar.objects.filter(status=1).order_by('-date_created')
-    template_name = 'cars.html'
-    paginated_by = 6
+        and separate the posts in pages, limiting the number of car posts in 6
+        in each page """
+    def get(self, request, *args, **kwargs):
+        """ return cars page """
+        post_car = PostCar.objects.filter(status=1).order_by('-date_created')
+        paginator = Paginator(post_car, 6)
+
+        page_number = request.GET.get('page')
+        page_car = paginator.get_page(page_number)
+        context = {
+            'cars': 'active',
+            'post_car': page_car
+        }
+        return render(request, 'cars.html', context)
