9c4a3959c1032ddb800ec4d31968e4ebf26b4d8d
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-18 20:07:47 +0000

    Add tooltip and modify card style to display edit and delete icons

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
-$('#copyright').text(new Date().getFullYear());
-
 let btn = $('.up-button');
 
+/* To show the current year */
+$('#copyright').text(new Date().getFullYear());
+
+/* Scroll to top button */
 $(window).scroll(function() {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
@@ -14,3 +16,7 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
+/* tooltip */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
