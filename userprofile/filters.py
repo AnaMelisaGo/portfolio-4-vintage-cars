dba97a8e0e4bb9b3ddf39ba96e633b9bcd954a65
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-25 19:55:13 +0000

    Edit label and set up placeholder of filter field in user profile page

--- userprofile/filters.py
+++ userprofile/filters.py
@@ -1,10 +1,14 @@
 import django_filters
 from django_filters import CharFilter, BooleanFilter
 from django_filters.widgets import BooleanWidget
+from django.forms.widgets import TextInput
 from cars.models import PostCar
 
 
 class CanRentWidget(BooleanWidget):
+    """
+    Change the choices in the dropdown choices when filtering cars for rent
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.choices = (
@@ -15,7 +19,10 @@ class CanRentWidget(BooleanWidget):
 class PostCarFilter(django_filters.FilterSet):
     """ filter """
     car_model_title = CharFilter(
-        field_name='car_model_title', lookup_expr='icontains'
+        field_name='car_model_title',
+        lookup_expr='icontains',
+        label='Car Model',
+        widget=TextInput(attrs={'placeholder': 'Car Model'}),
     )
     can_rent = BooleanFilter(widget=CanRentWidget)
 
