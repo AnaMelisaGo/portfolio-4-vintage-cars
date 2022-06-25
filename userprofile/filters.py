39480c1b0ecf08a663e9d2a6daae5205f821f938
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-25 05:30:37 +0000

    Add can rent field in filters to show data if post is for rent

--- userprofile/filters.py
+++ userprofile/filters.py
@@ -1,6 +1,15 @@
 import django_filters
-from django_filters import CharFilter
+from django_filters import CharFilter, BooleanFilter
+from django_filters.widgets import BooleanWidget
 from cars.models import PostCar
+
+
+class CanRentWidget(BooleanWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.choices = (
+            ("Unknown", ("All")), ("true", ("Yes")), ("false", ("No"))
+        )
 
 
 class PostCarFilter(django_filters.FilterSet):
@@ -8,11 +17,9 @@ class PostCarFilter(django_filters.FilterSet):
     car_model_title = CharFilter(
         field_name='car_model_title', lookup_expr='icontains'
     )
-    year_manufactured = CharFilter(
-        field_name='year_manufactured', lookup_expr='iexact'
-    )
+    can_rent = BooleanFilter(widget=CanRentWidget)
 
     class Meta:
         """ class meta filter """
         model = PostCar
-        fields = {'car_model_title', 'year_manufactured', 'status'}
+        fields = {'car_model_title', 'status', 'can_rent'}
