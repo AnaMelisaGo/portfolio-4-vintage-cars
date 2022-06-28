17e5f05bf75fbf3d5321c43ed3882579ac327506
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-28 22:14:10 +0000

    Update code

--- cars/urls.py
+++ cars/urls.py
@@ -6,10 +6,13 @@ urlpatterns = [
     path('car_lists/', views.CarPageView.as_view(), name='cars'),
     path('car_detail/<int:post_id>', views.CarDetail.as_view(
     ), name='car_detail'),
-    path('add_car/', login_required(views.AddCarPost.as_view()), name='add_car'),
+    path('add_car/', login_required(views.AddCarPost.as_view(
+    )), name='add_car'),
     path('edit_car/<int:post_id>', login_required(views.EditCarPost.as_view(
     )), name='edit_car'),
-    path('delete_car/<int:post_id>', login_required(views.DeleteCarPost.as_view(
-    )), name='delete_car'),
-    path('like/<int:post_id>', login_required(views.LikeCar.as_view()), name='like_car'),
+    path('delete_car/<int:post_id>', login_required(
+        views.DeleteCarPost.as_view()
+    ), name='delete_car'),
+    path('like/<int:post_id>', login_required(views.LikeCar.as_view(
+    )), name='like_car'),
 ]
--- events/urls.py
+++ events/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('event_list/', views.EventListView.as_view(), name='events'),
     path('event_detail/<int:pk>', views.EventDetail.as_view(
     ), name='event_detail'),
-    path('add_event/', login_required(views.AddEventView.as_view()), name='add_event'),
+    path('add_event/', login_required(views.AddEventView.as_view(
+    )), name='add_event'),
     path('edit_event/<int:pk>', login_required(views.EditEventView.as_view(
     )), name='edit_event'),
     path('delete_event/<int:pk>', login_required(views.DeleteEventView.as_view(
