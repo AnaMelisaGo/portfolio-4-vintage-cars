443f602107b0588c0f2d228ac8fc22629a9e468f
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-28 09:39:39 +0000

    Edit settings.py

--- vintagecars/settings.py
+++ vintagecars/settings.py
@@ -19,9 +19,7 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-#development = os.environ.get('DEVELOPMENT', False)
-
-DEVELOPMENT = False
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
