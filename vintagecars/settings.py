40571a2ef5f68493839fed1fdd6277e70ce81e46
AnaMelisaGo <melisago.rockn@gmail.com>
2022-06-29 04:35:21 +0000

    Perform modifications to clean code

--- vintagecars/settings.py
+++ vintagecars/settings.py
@@ -105,13 +105,6 @@ WSGI_APPLICATION = 'vintagecars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 # Use Heroku database as backend
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
